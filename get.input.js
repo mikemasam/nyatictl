f6c3406095fbd7195dd7fa25ada539e40e8710b3
sudo <meshackmasam@gmail.com>
2023-06-21 23:09:22 +0300

    optinal env? added

--- get.input.js
+++ get.input.js
@@ -7,9 +7,10 @@ const isEmpty = (str) => {
 export default async function getInput(client, opt) {
   const { label, name, required = true, hidden = false } = opt;
   let value = client.server[name];
-  value = await parseEnv(client, name, value);
-  //console.log(name, value);
+  const [next_value, optional] = await parseEnv(client, name, value);
+  value = next_value;
   if (!isEmpty(value)) return value;
+  if (optional) return undefined;
   value = await readInput(label, name, required, hidden);
   return value;
 }
@@ -33,14 +34,19 @@ async function readInput(label, name, required, hidden) {
 
 async function parseEnv(client, name, value) {
   let _env_name = null;
+  let optional = false;
   if (isEmpty(value)) _env_name = `NYATI_${name}`;
   if (!isEmpty(value) && value.toLowerCase().indexOf("#env:") == 0) {
     _env_name = `NYATI_${value.split("#env:")[1]}`;
+  } else if (!isEmpty(value) && value.toLowerCase().indexOf("#env?:") == 0) {
+    _env_name = `NYATI_${value.split("#env?:")[1]}`;
+    optional = true;
   } else {
-    return value;
+    return [value, optional];
   }
   let nvalue = client.env[_env_name];
-  if (nvalue == undefined) console.log(` ${_env_name} not defined`);
+  if (nvalue == undefined && !optional)
+    console.log(` ${_env_name} not defined`);
   //  console.log(
   //    "get: ",
   //    name,
@@ -51,5 +57,5 @@ async function parseEnv(client, name, value) {
   //    " value: ",
   //    nvalue
   //  );
-  return nvalue;
+  return [nvalue, optional];
 }
